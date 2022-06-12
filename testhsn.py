163a317739bc51311afe58a0ebbbfdd200d43e5f
Aymen <muhammedayman@gmail.com>
2022-06-12 13:31:42 +0530

    how to

--- testhsn.py
+++ testhsn.py
@@ -1,5 +1,6 @@
 from hsn.hsn_codes import aa
-for j in range(10):
-     for i,ii in aa[j]['tax_slab'].items():
-         if ii==1:
-             print('gst=',i,'hsn_code=',aa[j]['hsn_code'],'hsn_description=',aa[j]['hsn_description'])
+def test_code():
+    for j in range(10):
+        for i,ii in aa[j]['tax_slab'].items():
+            if ii==1:
+                print('gst=',i,'hsn_code=',aa[j]['hsn_code'],'hsn_description=',aa[j]['hsn_description'])
